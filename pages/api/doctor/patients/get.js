6f66497674542ce3aa540711f36e3982a18a3c72
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-01-23 03:54:29 +0530

    added getting history endpoints

---
+++ pages/api/doctor/patients/get.js
@@ -0,0 +1,33 @@
+import { connectToDatabase } from "../../../../models/db";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const token = req.body.token;
+    const id = req.body.id;
+    if (!token) {
+        res.status(400).json({ message: "Please enter token" });
+    }
+    const { db, users, doctors } = await connectToDatabase();
+    const result = await doctors.findOne({ token: token });
+    // console.log(result)
+    if (result) {
+        let medhis = result.medicalHistory;
+        let final;
+        for (let his of medhis) {
+            if (his.id === id) {
+                final = his;
+                break;
+            }
+        }
+        if (final) {
+            res.status(200).json({ message: "Data found", data: final });
+        }else{
+            res.status(200).json({ message: "Data not found" });
+        }
+    }
+    else {
+        res.status(201).json({ message: "Incorrect token" });
+        console.log('Incorrect token')
+    }
+  }
+}
