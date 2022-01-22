2417c45a05f89b1c90e25a866d64e6dae352de44
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-01-22 18:41:21 +0530

    added blog api routes

---
+++ pages/api/doctor/blog/get.js
@@ -0,0 +1,17 @@
+import { connectToDatabase } from "../../../../models/db";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    const token = req.headers.token;
+    const { db, users, doctors } = await connectToDatabase();
+    const doctor = await doctors.findOne({ token: token });
+    if (doctor){
+        const blogID = req.body.blogID;
+        const blog = await db.collection("blogs").findOne({ _id: blogID });
+        if (blog){
+            res.status(200).json({ blog: blog });
+        }
+      };
+    }
+    res.status(201)
+  }
