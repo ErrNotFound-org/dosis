9e74863b49423a394058b77e12d662572c489f6d
code123841 <udhay1co.de@gmail.com>
2022-01-22 16:50:48 +0530

    Doctor, patients login Is working without any bugs

--- models/db.js
+++ models/db.js
@@ -2,9 +2,10 @@ import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
   const client = await MongoClient.connect(
-    "mongodb+srv://DosisAdmin:ohyesnicenicepassword@dosisdata.cnont.mongodb.net"
+    "mongodb+srv://DosisAdmin:ohyesnicenicepassword@dosisdata.cnont.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
   );
-  const db = client.db();
+  const db = client.db("dosisdata");
+
   const users = db.collection("users");
   const doctors = db.collection("Doctor");
   return { db, users, doctors };
